f2fc7b2946935690a5cdf2f630dfc55f4afa58f8
Shobhit Makhija <shobhitmakhija@Shobhits-MacBook-Pro.local>
2021-04-03 18:06:33 -0400

    changed to html for better read

--- trailStatus.py
+++ trailStatus.py
@@ -3,9 +3,10 @@ import requests
 def getWebPage():
 
     info = requests.get("https://www.trianglemtb.com/mobiletrailstatus.php")
-    print(info.content)
 
-    with open('OUTPUT.txt','w') as outfile:
+    with open('OUTPUT.html','w') as outfile:
         outfile.write(info.text)
 
 getWebPage()
+
+
