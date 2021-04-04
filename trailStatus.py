d6ce45b50c074c22295ee9ff41c98e94d637f1c6
Shobhit Makhija <shobhitmakhija@Shobhits-MacBook-Pro.local>
2021-04-03 22:00:09 -0400

    Extract all trail names

--- trailStatus.py
+++ trailStatus.py
@@ -1,11 +1,19 @@
 import requests
+from bs4 import BeautifulSoup
 
 def getWebPage():
 
     info = requests.get("https://www.trianglemtb.com/mobiletrailstatus.php")
+    soup = BeautifulSoup(info.content, 'html.parser')
+
+    results = soup.findAll('a')
 
     with open('OUTPUT.html','w') as outfile:
-        outfile.write(info.text)
+        for x in results:
+
+            final_str =  x.contents[0].strip()
+            print(final_str)
+            outfile.write(final_str + "\n")
 
 getWebPage()
 
